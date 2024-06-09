fa95f01dbdf995ac5b707d919dfb587fd0472d51
SaschaBorgesLucas <saschaborgeslucas@gmail.com>
2024-06-09 19:31:35 -0400

    sascha 9/6/24 - 0: adicionada rota de listagem de filmes)

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (request, response) =>{
-    response.send("listagem de filmes");
+app.get("/movies", async (request, response) =>{
+    const movie = await prisma.movie.findMany();
+    response.json(movie)
 });
 
 
